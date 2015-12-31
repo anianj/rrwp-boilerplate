41e7868db0075efb1e858b9ad6934550ad734ee7
nian jian <nianj@newdeMacBook-Pro-2.local>
2015-12-31 14:18:20 +0800

    remove build files from dist

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -2,6 +2,7 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    displayName: "Application",
     getInitialState: function(){
         return {count:0}
     },
@@ -10,6 +11,6 @@ module.exports = React.createClass({
         this.setState({count: this.state.count+1});
     },
     render: function(){
-        return (<a href="https://www.baidu.com" onClick={this.onClick}>Hello world!!! {this.state.count}</a>)
+        return (<a href="https://www.baidu.com" onClick={this.onClick}>Hello world! {this.state.count}</a>)
     }
 });
